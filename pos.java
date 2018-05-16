cdda6ddc5ee143e5a87ec99419a0d7e625cfde0e
Aishu6897 <36623642+Aishu6897@users.noreply.github.com>
2018-05-16 14:36:19 +0530

    Add files via upload

---
+++ pos.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+class pos
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	Scanner s=new Scanner(System.in);
+	
+           int n=s.nextInt();
+	
+if(n<1)
+
+	System.out.println("negative");
+	
+else 
+	
+System.out.println("positive");
+	}
+
+}
