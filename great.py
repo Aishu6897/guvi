cc7bb6878ec1b5b4347e02a6f6a3c8b19edb1a89
Aishu6897 <36623642+Aishu6897@users.noreply.github.com>
2018-05-17 10:32:27 +0530

    Add files via upload

---
+++ great.py
@@ -0,0 +1,17 @@
+wh=int(input())
+
+wh1=int(input())
+
+wh2=int(input())
+
+if(wh>wh1 and wh>wh2):
+    
+       print wh
+
+elif(wh1>wh and wh1>wh2):
+    
+        print wh1 
+
+else:
+      
+        print wh2
